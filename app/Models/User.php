ef90de32ff2eadb645696277e75c9bbf9cd96e86
ilhamsamsularifin <ilhamsamsularifin@gmail.com>
2025-05-21 10:22:02 +0700

    add model and migration ticketReply

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,4 +51,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Ticket::class);
     }
+
+    public function ticketReplies()
+    {
+        return $this->hasMany(TicketReply::class);
+    }
 }
