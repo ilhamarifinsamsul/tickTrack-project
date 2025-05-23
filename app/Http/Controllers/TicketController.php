52d164e7ae59e1ec07e6573937f92de47d4af041
ilhamsamsularifin <ilhamsamsularifin@gmail.com>
2025-05-23 15:54:42 +0700

    add function api get all data

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -11,6 +11,41 @@ use Illuminate\Support\Facades\DB;
 
 class TicketController extends Controller
 {
+    public function index(Request $request) {
+        try {
+            $query = Ticket::query();
+        $query->orderBy('created_at', 'desc');
+
+        if ($request->search) {
+            $query->where('code', 'like', '%' . $request->search . '%')->orWhere('title', 'like', '%' . $request->search . '%');
+        }
+
+        if ($request->status) {
+            $query->where('status', $request->status);
+        }
+
+        if ($request->priority) {
+            $query->where('priority', $request->priority);
+        }
+
+        if (auth()->user()->role == 'user') {
+            $query->where('user_id', auth()->user()->id);
+        }
+
+        $tickets = $query->get();
+        return response()->json([
+            'message' => 'List Tiket Berhasil Diambil',
+            'data' => TicketResource::collection($tickets) // Jika data banyak, gunakan collection
+        ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Terjadi kesalahan',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+
     public function store(TictekStoreRequest $request)
     {
         // Validate the request
@@ -32,7 +67,7 @@ class TicketController extends Controller
 
             return response()->json([
                 'message' => 'Ticket created successfully',
-                'data' => new TicketResource($ticket)
+                'data' => new TicketResource($ticket) // Jika data satu, gunakan new
             ], 201);
 
         } catch (Exception $e) {
